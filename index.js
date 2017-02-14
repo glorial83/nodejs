51dae1f00d215dc3fdca286d2b49361784b3c60f
glorial <glorial@naver.com>
2017-02-14 09:04:42 +0900

    npm기본패키지인  path 사용

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var express = require("express");
+var path    = require("path");
+var app     = express();
 
-var app = express();
-
-app.get('/', function(req,res){
-    res.send("Hello World!");
-  }
-);
+//"public" 폴더를 최초폴더로 사용하겠다는 의미같음
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, function(){
     console.log("Server On!!!!!!");
