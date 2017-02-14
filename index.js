169ee5ba6d221dcccf70f3673a89e8f7af5032e3
glorial <glorial@naver.com>
2017-02-14 09:19:22 +0900

    EJS(Embedded Javascript) 패키지를 사용한 스크립팅

--- index.js
+++ index.js
@@ -6,6 +6,34 @@ var app     = express();
 //app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//view engine 으로 ejs를 설정
+app.set("view engine", "ejs");
+
+var data = {count:0};
+app.get("/", function(req, res){
+    data.count++;
+    res.render("my_first_ejs", data);
+  }
+);
+app.get("/reset", function(req, res){
+    data.count = 0;
+    res.render("my_first_ejs", data);
+  }
+);
+app.get("/set/count", function(req, res){
+    if(req.query.count){
+      data.count = req.query.count;
+    }
+
+    res.render("my_first_ejs", data);
+  }
+);
+app.get("/set/:num", function(req, res){
+    data.count = req.params.num;
+    res.render("my_first_ejs", data);
+  }
+);
+
 app.listen(3000, function(){
     console.log("Server On!!!!!!");
   }
